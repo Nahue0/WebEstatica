c13e6d8eca6427a4966a2c406262bbe6179f6510
Nahue0 <luis.oliva@est.fi.uncoma.edu.ar>
2022-10-01 18:36:41 -0300

    commit

--- java.js
+++ java.js
@@ -1,8 +1,4 @@
-        function abrir_ventana(){
-            window.open("https://www.youtube.com/watch?v=YbxADoU1yK8&ab_channel=WmediaporJuanAndr%C3%A9sN%C3%BA%C3%B1ez");
+    obtenerDiv = document.getElementById("contenedorFlex");
+    console.log(obtenerDiv);
+    console.log('Hola mundo');
 
-        }
-        function parar_abrir(){
-            clearTimeout(pop);
-        }
-        pop = setInterval("abrir_ventana()",3000);
