4a3308621913fd2e493b1e260cc7269ef47f63f7
Nahue0 <luis.oliva@est.fi.uncoma.edu.ar>
2022-10-03 07:42:36 -0300

    se agrego las animaciones a la pagina principal

--- java.js
+++ java.js
@@ -1,4 +1,38 @@
-()=>{
- const listas = document.getElementById('contenedorFlex') ;  
- console.log(listas);
-};
+$(document).ready(function(){
+    let lista = $('.circulos');
+    console.log(lista.length);    
+    $(lista).hide();
+    $(lista[0]).show();
+    let i = 0;
+
+    $('#adelante').click(
+        function(){
+                if(i < lista.length-1){
+                    i = i + 1;
+                    $(lista).hide();
+                    $(lista[i]).show();
+                }else{
+                    i = 0;
+                    $(lista).hide();
+                    $(lista[i]).show();
+                }
+
+        }
+    );
+
+    $('#atras').click(
+        function(){
+                if(i > 0){
+                    i = i - 1;
+                    $(lista).hide();
+                    $(lista[i]).show();
+                }else{
+                    i = lista.length-1;
+                    $(lista).hide();
+                    $(lista[i]).show();
+                }
+
+        }
+    );
+
+});
