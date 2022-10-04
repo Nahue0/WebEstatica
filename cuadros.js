5bd33ef4dd90454cac20203ba17554e05ccdb108
Nahue0 <luis.oliva@est.fi.uncoma.edu.ar>
2022-10-04 15:57:56 -0300

    se agregaron imagenes para las batallas - se cambio el cuadro por un div+gird

---
+++ cuadros.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    let mostrar  = $('.mostrar')
+    let texto = document.getElementsByName('tablaD');
+    let gokuNube = $('#gokuNube');
+    $(texto).hide();
+    
+    $(mostrar[0]).on('click',function(mostrar){
+        if($(texto[0]).is(':visible')){
+            $(texto[0]).hide();
+        }else{
+            $(texto).hide();
+            $(texto[0]).show();
+        }
+    });
+
+    $(mostrar[1]).on('click',function(){
+        if($(texto[1]).is(':visible')){
+            $(texto[1]).hide();
+        }else{
+            
+            $(texto).hide();
+            $(texto[1]).show();
+        }
+    });
+
+    $(mostrar[2]).on('click',function(){
+        if($(texto[2]).is(':visible')){
+            $(texto[2]).hide();
+            $(gokuNube).show();
+            $('body').css('background-image','url(./imagenes/windowsGoku.webp)');
+        }else{
+            $(texto).hide();
+            $(texto[2]).show();
+            $(gokuNube).hide();
+            $('body').css('background-image','url(./imagenes/peleas/gokuVsVegetta.jpg)');
+        }
+    });
+
+    
+});
